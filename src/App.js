0dd94e88e9541ac67f540124cb4656d12ac6c85d
Junior Gabi <juniorgabi@juniors-air.home>
2022-12-09 11:58:41 +0000

    links working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Header } from "./components/header";
-import "./app.css"
+import "./App.css"
 import { Navigation } from "./components/navigation";
 import { Main } from "./components/Main";
+import { Routes,Route } from "react-router-dom";
+import { IndividualArticle } from "./components/IndividualArticle";
 
 function App() {
   return (<div className="container">
     <div className="container2">
     <Header/>
     <Navigation/>
-    <Main/>
+    <Routes>
+      <Route path ="/" element={<Main/>}/>
+      <Route path ="/home" element={<IndividualArticle/>}/>
+    </Routes>
     </div>
   </div>
     
---
+++ src/components/IndividualArticle.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const IndividualArticle = () => {
+  return (
+    <div>IndividualArticle</div>
+  )
+}
